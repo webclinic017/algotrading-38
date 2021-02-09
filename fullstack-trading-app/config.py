89685049e827dfd6d7191e4f4441c799a3028312
amitrahman1026 <70333123+amitrahman1026@users.noreply.github.com>
2021-02-09 17:27:47 +0800

    Update config.py

--- fullstack-trading-app/config.py
+++ fullstack-trading-app/config.py
@@ -3,12 +3,12 @@ cwd = os.getcwd()
 
 
 API_URL = 'https://paper-api.alpaca.markets'
-API_KEY = 'PK8BQP15JXZB9D1FZ3EM'
-SECRET_KEY = 'dx4RYzjPkX6IXQTsRibg3Sg0rhA4QaG8UBfiKPdh'
+API_KEY = ''
+SECRET_KEY = ''
 
 DB_FILE = os.path.join(cwd, "app.db")
 
-EMAIL_ADDRESS = 'amit.amit.rahman2@gmail.com'
-EMAIL_PASSWORD = '94814937'
+EMAIL_ADDRESS = ''
+EMAIL_PASSWORD = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
